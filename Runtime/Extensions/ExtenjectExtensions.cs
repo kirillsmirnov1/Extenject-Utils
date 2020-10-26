b0da50c2cda7de29cf621160d5d3439c4ee42a15
Kirill Smirnov <kirill@dg-line.ru>
2020-10-26 11:21:57 +0300

    ExtenjectExtensions.cs

---
+++ Runtime/Extensions/ExtenjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+
+namespace ExtenjectUtils.Extensions
+{
+    public static class ExtenjectExtensions
+    {
+        public static T InjectBy<T>(this T obj,  DiContainer container)
+        {
+            container.Inject(obj);
+            return obj;
+        }
+    }
+}
